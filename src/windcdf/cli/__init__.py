c25fa3932331801c8c9ba2fc016ac1ccd6a52630
Miguel Cordoba <miguel.cordoba@oceanwinds.com>
2026-02-09 08:14:16 +0100

    muy simple

--- src/windcdf/cli/__init__.py
+++
@@ -1,7 +0,0 @@
-"""CLI for WindCDF."""
-
-from windcdf.qc_tool.app import main
-
-
-if __name__ == "__main__":
-    main()
--- src/windcdf/qc_tool/__init__.py
+++
@@ -1,5 +0,0 @@
-"""WindCDF Interactive QC Tool - Tkinter-based NetCDF quality control application."""
-
-from windcdf.qc_tool.app import QCToolApp
-
-__all__ = ["QCToolApp"]
